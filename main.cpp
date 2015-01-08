182ed1bf5630cead5c1e7a99a652c9dc365a90b1
Mazar93 <mast0047@stud.hs-kl.de>
2015-01-08 14:39:12 +0100

    Fixed

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include "MatrixMul.h"
 #include "MatrixMulOMP.h"
 #include "MatrixMulCilk.h"
 #include "MatrixMulTBB.h"
+
 
 
 #define MAX 20
@@ -14,39 +16,40 @@ using namespace std;
 int main() {
 
 
-	int height, width;
+	int dim;
 	string form;
 	 
 
-	cout << "Zeilen der Matrix eingeben:  ";
-	cin >> height;
-	cout << "Spalten der Matrix eingeben:  ";
-	cin >> width;
+	cout << "Dimension der Matrix eingeben:  ";
+	cin >> dim;
+	
 
 	cout << "Bitte eingeben:" << endl;
-	cout << "1 fuer OpenMP" << endl;
-	cout << "2 fuer Cilk" << endl;
-	cout << "3 fuer TBB" << endl;
+	cout << "1 fuer normal" << endl;
+	cout << "2 fuer OpenMP" << endl;
+	cout << "3 fuer Cilk" << endl;
+	cout << "4 fuer TBB" << endl;
 	cin >> form;
 
 
-	
 	if (form == "1"){
-			MatrixMulOMP omp;
-			omp.setHeigth(height);
-			omp.setWidth(width);
-			omp.start();
+			MatrixMul none;
+			none.setDim(dim);
+			none.start();
 		}
 	else if (form == "2"){
-			MatrixMulCilk cilk;
-			cilk.setHeigth(height);
-			cilk.setWidth(width);
-			cilk.start();
+			MatrixMulOMP omp;
+			omp.setDim(dim);
+			omp.start();
 		}
 	else if (form == "3"){
+			MatrixMulCilk cilk;
+			cilk.setDim(dim);
+			cilk.start();
+		}
+	else if (form == "4"){
 			MatrixMulTBB tbb;
-			tbb.setHeigth(height);
-			tbb.setWidth(width);
+			tbb.setDim(dim);
 			tbb.start();
 		}
 
